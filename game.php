db4915ca06108c6a265fd6fa28260a6826f4ab6c
kmh12c <kaylaholcomb@Kaylas-MacBook-Pro.local>
2017-03-03 09:24:12 -0600

    mysql and mysqli - track users

--- game.php
+++ game.php
@@ -16,8 +16,44 @@ if (isset($_REQUEST['long']))
 
 if ($msg == "start")
 {
-	$greeting = "welcome";
-	$playerId = 1;
+	$greeting = "Welcome";
+	$playerId = 0; //default
+
+	//$link = new mysqli("localhost","ec2-user", NULL,"mc2");
+	$link = new mysqli("localhost","ec2-user", NULL,"mc2");
+    if ($link->connect_errno) 
+    {
+        printf("Connect failed: %s\n", $link->connect_error);
+        exit();
+    }
+    else //mysqli connection successful
+    {
+        $result = $link->query("SELECT * FROM users WHERE ip='$ip'");
+        if(!$result)
+            die ('Can\'t query users because: ' . $link->error);
+        else //query successful
+    	{
+    		$num_rows = mysqli_num_rows($result);
+	        if ($num_rows > 0) //existing playerId for that IP address
+	        {
+	        	$greeting = "Welcome Back";
+	        	$player = $result->fetch_assoc();
+	        	$playerId = $player["playerId"];
+	        }
+	        else //no existing playerId for that IP
+	        {
+	        	$result = $link->query("INSERT INTO users (ip) VALUES ('$ip')");
+				if(!$result)
+       				die ('Can\'t add user because: ' . $link->error);
+       			$result = $link->query("SELECT * FROM users WHERE ip='$ip'");
+   				if(!$result)
+        			die ('Can\'t query users because: ' . $link->error);
+        		$player = $result->fetch_assoc();
+	        	$playerId = $player["playerId"];
+	        }
+		}
+	}
+
 	$info = ["blee", "blah"];
 	$things = array($greeting, $playerId, $info);
 }
