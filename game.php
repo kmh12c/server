e60b7381ea827fd4769b42d12541e9100eced4ef
kmh12c <kmh12c@acu.edu>
2017-03-10 13:37:30 -0600

    fake walking update

--- game.php
+++ game.php
@@ -5,38 +5,82 @@
 // respond to GET
 //
 
-$ip = $_SERVER['REMOTE_ADDR'];
+// Function to get the client ip address
+function getRealIpAddr()
+{
+    if (!empty($_SERVER['HTTP_CLIENT_IP']))   //check ip from share internet
+    {
+      $ip=$_SERVER['HTTP_CLIENT_IP'];
+    }
+    elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))   //to check ip is pass from proxy
+    {
+      $ip=$_SERVER['HTTP_X_FORWARDED_FOR'];
+    }
+    else
+    {
+      $ip=$_SERVER['REMOTE_ADDR'];
+    }
+    return $ip;
+}
+
+$ip = getRealIpAddr();
 $msg = $_GET["msg"];
 
-if (isset($_REQUEST['long']))
+$target_lat = 32.4675765;
+$target_lon = -99.70698;
+$target_delta = 10.0;
+$meter1 = .00001;
+
+
+if (isset($_REQUEST['lon']))
 {
-	$long = $_GET["long"];
+	$lon = $_GET["lon"];
 	$lat = $_GET["lat"];
 }
 
-if ($msg == "start")
-{
-	$greeting = "Welcome";
+function distance($lat1, $lon1, $lat2, $lon2) {
+  $latMid = ($lat1+$lat2 )/2.0;  // or just use Lat1 for slightly less accurate estimate
+
+  $m_per_deg_lat = 111132.954 - 559.822 * cos( 2.0 * $latMid ) + 1.175 * cos( 4.0 * $latMid);
+  $m_per_deg_lon = (3.14159265359/180 ) * 6367449 * cos ( $latMid );
+
+  $deltaLat = abs($lat1 - $lat2);
+  $deltaLon = abs($lon1 - $lon2);
+
+  $dist_m = sqrt (  pow( $deltaLat * $m_per_deg_lat,2) + pow( $deltaLon * $m_per_deg_lon , 2) );
+  //  print ('distance '. $lat1. ',' .$lon1. ' to '. $lat2. ','. $lon2. ' is '. $dist_m. "\n");
+  return $dist_m;
+}
+
+function closeEnough ($lat, $lon) {
+  GLOBAL $target_lat, $target_lon, $target_delta;
+  return  distance($lat, $lon, $target_lat, $target_lon) < $target_delta;
+}
+
+switch ($msg) {
+
+case "start":
+	error_log("start received", 0);
+	$msg = 'welcome';
 	$playerId = 0; //default
 
-	//$link = new mysqli("localhost","ec2-user", NULL,"mc2");
 	$link = new mysqli("localhost","ec2-user", NULL,"mc2");
-    if ($link->connect_errno) 
-    {
-        printf("Connect failed: %s\n", $link->connect_error);
-        exit();
-    }
-    else //mysqli connection successful
-    {
-        $result = $link->query("SELECT * FROM users WHERE ip='$ip'");
-        if(!$result)
-            die ('Can\'t query users because: ' . $link->error);
-        else //query successful
-    	{
-    		$num_rows = mysqli_num_rows($result);
+	if ($link->connect_errno) 
+	{
+	    printf("Connect failed: %s\n", $link->connect_error);
+	    exit();
+	}
+	else //mysqli connection successful
+	{
+	    $result = $link->query("SELECT * FROM users WHERE ip='$ip'");
+	    if(!$result)
+	        die ('Can\'t query users because: ' . $link->error);
+	    else //query successful
+		{
+			$num_rows = mysqli_num_rows($result);
 	        if ($num_rows > 0) //existing playerId for that IP address
 	        {
-	        	$greeting = "Welcome Back";
+	        	$msg = "welcome back";
 	        	$player = $result->fetch_assoc();
 	        	$playerId = $player["playerId"];
 	        }
@@ -44,29 +88,34 @@ if ($msg == "start")
 	        {
 	        	$result = $link->query("INSERT INTO users (ip) VALUES ('$ip')");
 				if(!$result)
-       				die ('Can\'t add user because: ' . $link->error);
-       			$result = $link->query("SELECT * FROM users WHERE ip='$ip'");
-   				if(!$result)
-        			die ('Can\'t query users because: ' . $link->error);
-        		$player = $result->fetch_assoc();
+	   				die ('Can\'t add user because: ' . $link->error);
+	   			$result = $link->query("SELECT * FROM users WHERE ip='$ip'");
+					if(!$result)
+	    			die ('Can\'t query users because: ' . $link->error);
+	    		$player = $result->fetch_assoc();
 	        	$playerId = $player["playerId"];
-	        }
-		}
+	    	}
+	    }
 	}
+	
+	$things = array( 'msg'=> $msg, 'id' => $playerId, 'latitude'=>$target_lat, 'longitude'=>$target_lon, 'ip' => $ip);
+break;
 
-	$info = ["blee", "blah"];
-	$things = array($greeting, $playerId, $info);
-}
-elseif ($msg == "walking")
-{
-	$greeting = "walking";
-	$things = array($greeting, $long, $lat);
-}
-else
-{
-	$greeting = "win";
-	$info = ["", ""];
-	$things = array($greeting, $info);
+case "walking":
+	error_log("walking received", 0);
+	$distance = distance($lat, $lon, $target_lat, $target_lon);
+	if ($distance < $target_delta) 
+	{
+		$things = array( 'msg'=> 'win', 'latitude'=>$lat, 'longitude'=>$lon, 'ip' => $ip);
+	} 
+	else 
+	{
+		$things = array( 'msg'=> 'keepwalking', 'latitude'=>$lat, 'longitude'=>$lon, 'distance'=>$distance, 'ip' => $ip);
+	}
+break;
+
+default:
+  $things = array( 'msg'=> 'speakbetter', 'ip' => $ip);
 }
 
 header("Content-type: application/json");
